ebc91e0234483b2e0d9108de9bb7e326477e81e9
bmerkle3 <braidymerkle@gmail.com>
2017-10-12 15:30:13 -0500

    address layout on new tweet page

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'activesupport'
 
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 20171012192346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "friendships", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "friend_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
