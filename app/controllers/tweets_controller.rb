8d46860612be43dc09e7a7d5e244e04ed2c69c36
bmerkle3 <braidymerkle@gmail.com>
2017-10-12 13:21:13 -0500

    add link to profile page for new tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,28 @@
 class TweetsController < ApplicationController
   def new
+    @tweet = Tweet.new
   end
 
   def index
+    # !!!should be all tweets of all friendships ordered chronologically
+    @tweets = Tweet.all
   end
 
   def show
+  end
+
+  def create
+    @tweet = Tweet.new(permit_tweet)
+    if @tweet.save
+      redirect_to tweet_path(@tweet)
+    else
+      flash[:error] = @tweet.errors.full_messages
+      redirect_to new_tweet_path
+    end
+  end
+
+  private
+  def permit_tweet
+    params.require(:tweet).permit(:body, :user_id)
   end
 end
